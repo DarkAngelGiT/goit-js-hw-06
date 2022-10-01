ad9842bdce39c09faaba2f72c8f06eef6e417415
DarkAngelGiT <dark.angel.wot@gmail.com>
2022-10-01 11:14:44 +0300

    hw-6 start

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+// const ulItem = document.querySelector("#categories")
+// console.log(ulItem);
+const liItem = document.querySelectorAll("li.item")
+console.log("Number of categories: ", liItem.length);
+
