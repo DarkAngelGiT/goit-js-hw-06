13f2ae973152992d88448c8a44af0241737595c5
DarkAngelGiT <dark.angel.wot@gmail.com>
2022-10-01 14:08:22 +0300

    Task-01 Done

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,11 @@
-// const ulItem = document.querySelector("#categories")
-// console.log(ulItem);
-const liItem = document.querySelectorAll("li.item")
-console.log("Number of categories: ", liItem.length);
+
+const categoryItem = document.querySelectorAll("li.item")
+console.log("Number of categories:", categoryItem.length);
+
+categoryItem.forEach(categ => {
+  const liItem = categ.querySelector("h2");
+  const itemCount = categ.querySelectorAll("li")
+  console.log(`Category:`, liItem.textContent);
+  console.log(`Elements:`, itemCount.length);
+})
 
