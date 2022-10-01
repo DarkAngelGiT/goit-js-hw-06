c2ce719201aeffa8688f115a065a9d7ba3a84794
DarkAngelGiT <dark.angel.wot@gmail.com>
2022-10-02 00:05:49 +0300

    Task-02 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,16 @@
-
-const categoryItem = document.querySelectorAll("li.item")
+// знаходимо і повертаємо масив елементів з класом item
+const categoryItem = document.querySelectorAll("li.item");
+// знаходимо довжину масиву
 console.log("Number of categories:", categoryItem.length);
 
+// перебираємо масив елементів categoryItem
 categoryItem.forEach(categ => {
+  // знаходимо масив дочірніх елементів тегів h2
   const liItem = categ.querySelector("h2");
-  const itemCount = categ.querySelectorAll("li")
+  // виводимо textContent заголовків знайденого масиву
   console.log(`Category:`, liItem.textContent);
+  // знаходимо масив дочірніх елементів тегів li
+  const itemCount = categ.querySelectorAll("li");
+  // виводимо довжину знайденого масиву
   console.log(`Elements:`, itemCount.length);
-})
-
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,34 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// const creatElem = document.createElement("li");
+// creatElem.textContent = ingredients[0];
+// creatElem.classList.add("item");
+// console.log(creatElem);
+
+// знаходимо і повертаємо елемент з ІD ingredients
+const ingredientsRef = document.querySelector("#ingredients")
+
+// створюємо стрілочну ф-цію для створення елементів сприску
+const createIngredients = (ingredient) => {
+  // перебираємо масив значень ingredients 
+  return ingredient.map((item) => {
+    // створюємо новий еле-нт
+    const creatElem = document.createElement("li");
+    // додаємо значення елементу масиву як текст в новий елемент списку
+    creatElem.textContent = item;
+    // додаємо клас елементу списку
+    creatElem.classList.add("item");
+    // повертаємо створений елемент
+    return creatElem;
+  });
+};
+
+// запуск створення елементів
+const elements = createIngredients(ingredients);
+
+// додаємо елементи на сторінку html + збираємо масив елементів за допомогою ...spread
+ingredientsRef.append(...elements)
+
+console.log(ingredientsRef);
