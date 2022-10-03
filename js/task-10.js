8691b9b131209ada6e13953cdcf16d29b0580944
DarkAngelGiT <dark.angel.wot@gmail.com>
2022-10-03 23:19:59 +0300

    Upd task-10
    task in progress...

--- js/task-10.js
+++ js/task-10.js
@@ -1,4 +1,11 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
 }
+
+const inputRef = document.querySelector("#controls input"); // звернення до інпуту
+const createBoxBtn = document.querySelector("[data-create]"); // звернення до кнопки створення елементів
+const deleteBoxBtn = document.querySelector("[data-destroy]"); // звернення до кнопки видалення елементів
+const boxes = document.querySelector("#boxes"); // звернення до діву куди будемо записувати створені елементи
+let baseBoxSize = 30; // змінна початкового розміру елементів
 
