264999f5790d67272f4e1810d1278f8ad1f04dde
DarkAngelGiT <dark.angel.wot@gmail.com>
2022-10-03 21:58:27 +0300

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorRef = document.querySelector(".color");
+const changeColorRef = document.querySelector(".change-color");
+const changeCurrentElem = document.querySelector(".widget");
+// const changeCurrentElem = document.querySelector("body");
+
+const changeColor = () => {
+  const randomColor = getRandomHexColor();
+  changeCurrentElem.style.backgroundColor = randomColor;
+  colorRef.textContent = randomColor;
+};
+
+changeColorRef.addEventListener("click", changeColor);
